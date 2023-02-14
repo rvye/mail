f19d195019314df72f91adfd794b089054876654
rye <114079592+rvye@users.noreply.github.com>
2023-02-14 17:01:03 -0600

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,4 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Importing necessary libraries
 import sys
-import tkinter as tk
+import tkinter as tk 
 import smtplib as sm
 from tkinter import ttk
+
+# Variables, used later
+saved = False
+
+# Setting up tkInter window
+root = tk.Tk()
+root.geometry("200x100")
+root.resizable(False, False)
+root.title("mail")
+
+# window destroying function
+def clearWindow(*widgets):
+    for widget in widgets:
+        # Exits each window defined in *widgets
+        widget.destroy
+
+def EmailPrompt():
+
+
+
+# Labeling
+welcomeLabel = ttk.Label(
+    root,
+    text="Mail")
+
+detailLabel = ttk.Label(
+    root,
+    text="Are your details saved?")
+
+yesButton = tk.Button(
+    root,
+    text="Yes"
+    command=lambda: clearWindow(root)
+)
+
